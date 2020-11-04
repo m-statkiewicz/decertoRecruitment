e6848691bb3e2f575cd60adfd1fee64d6770c60b
Mateusz Statkiewicz <mat.statkiewicz@gmail.com>
2020-11-05 00:20:53 +0100

    Initializr

---
+++ src/main/java/com/mstatkiewicz/decertoRecruitment/DecertoRecruitmentApplication.java
@@ -0,0 +1,13 @@
+package com.mstatkiewicz.decertoRecruitment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecertoRecruitmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DecertoRecruitmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mstatkiewicz/decertoRecruitment/DecertoRecruitmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mstatkiewicz.decertoRecruitment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecertoRecruitmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
